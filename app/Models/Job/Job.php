29b89233206478f10f5d40e5b4e0343a11389d41
kh-abrar <kh.abrarr@gmail.com>
2023-09-21 00:37:53 +0600

    first commit

---
+++ app/Models/Job/Job.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\Job;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jobs';
+
+    protected $fillable = [
+        'id',
+        'job_title',
+        'job_region',
+        'company',
+        'job_type',
+        'vacancy',
+        'experience',
+        'salary',
+        'Gender',
+        'application_deadline',
+        'jobdescription',
+        'responsibilities',
+        'education_experience',
+        'otherbenefits',
+        'image',
+    ];
+
+    public $timestamps = true;
+}
