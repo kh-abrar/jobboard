9bb183f28bd36cab8362681ae62492ade19d0885
kh-abrar <kh.abrarr@gmail.com>
2023-09-22 14:56:09 +0600

    saving jobs done

--- app/Http/Controllers/Jobs/JobsController.php
+++ app/Http/Controllers/Jobs/JobsController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Jobs;
 
 use App\Http\Controllers\Controller;
 use App\Models\Job\Job;
+use App\Models\Job\JobSaved;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class JobsController extends Controller
 {
@@ -24,6 +26,28 @@ class JobsController extends Controller
         ->where('id', '!=', $id)
         ->count();
 
-        return view('jobs.single', compact('job', 'relatedJob', 'relatedJobCount'));
+
+        //Save Job
+        $savedJob = JobSaved::where('job_id', $id)
+        ->where('user_id', Auth::user()->id)
+        ->count();
+
+        return view('jobs.single', compact('job', 'relatedJob', 'relatedJobCount', 'savedJob'));
+    }
+
+    public function saveJob(Request $request){
+        $saveJob = JobSaved::create([
+            'job_id' => $request->job_id,
+            'user_id' => $request->user_id,
+            'job_image' => $request->job_image,
+            'job_title' => $request->job_title,
+            'job_region' => $request->job_region,
+            'job_type' => $request->job_type,
+            'company' => $request->company
+        ]);
+
+        if($saveJob){
+            return redirect('/jobs/single/'.$request->job_id.'')->with('save', 'Job Saved Successfully');
+        }
     }
 }
--- resources/views/jobs/single.blade.php
+++ resources/views/jobs/single.blade.php
@@ -16,7 +16,14 @@
           </div>
         </div>
       </section>
-  
+
+      <div class="container">
+        @if(\Session::has('save'))
+          <div class="alert alert-success">
+              <p>{!! \Session::get('save') !!}</p>
+          </div>
+        @endif
+      </div>
       
       <section class="site-section">
         <div class="container">
@@ -63,7 +70,23 @@
   
               <div class="row mb-5">
                 <div class="col-6">
-                  <button class="btn btn-block btn-light btn-md"><i class="icon-heart"></i>Save Job</button>
+                  <form action="{{route('save.job')}}" method="POST">
+                    @csrf
+                    <input name="job_id" type="hidden" value="{{$job->id}}">
+                    <input name="user_id" type="hidden" value="{{Auth::user()->id}}">
+                    <input name="job_image" type="hidden" value="{{$job->image}}">
+                    <input name="job_title" type="hidden" value="{{$job->job_title}}">
+                    <input name="job_region" type="hidden" value="{{$job->job_region}}">
+                    <input name="job_type" type="hidden" value="{{$job->job_type}}">
+                    <input name="company" type="hidden" value="{{$job->company}}">
+                  
+                  @if ($savedJob > 0)
+                    <button class="btn btn-block btn-success btn-md" disabled>Saved Already</button>
+                  @else
+                    <button name="submit" type="submit" class="btn btn-block btn-light btn-md">Save Job</button>
+                  @endif
+
+                  
                   <!--add text-danger to it to make it read-->
                 </div>
                 <div class="col-6">
